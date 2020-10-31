f3f21d2ad88a59ca1ecd5566d421c4a3a37498a0
venkatraj-web <73744647+venkatraj-web@users.noreply.github.com>
2020-10-31 18:11:32 +0530

    Constructoquest page designed

---
+++ constructoquest/js/theme.js
@@ -0,0 +1,21 @@
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+ $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
